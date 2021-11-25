3fa0279f5b1989751363640a312277fbf93572e3
Infarh <shmachilin@yandex.ru>
2021-11-25 21:14:42 +0300

    Блокировки и синхронизация - начало

--- UI/Publications.WPF/MainWindow.xaml.cs
+++ UI/Publications.WPF/MainWindow.xaml.cs
@@ -1,28 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Publications.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var calculation_thread = new Thread(() => UpdateData());
+            calculation_thread.Start();
+        }
+
+        private /*async*/ void UpdateData()
+        {
+            var result = CalculateData();
+            //ResultTextBlock.Text = result;
+            //ResultTextBlock.Dispatcher.Invoke(() =>
+            //{
+            //    ResultTextBlock.Text = result;
+            //});
+            //Application.Current.Dispatcher.BeginInvoke(() =>
+            //{
+            //    ResultTextBlock.Text = result;
+            //});
+            var operation = Application.Current.Dispatcher.InvokeAsync(() =>
+            {
+                ResultTextBlock.Text = result;
+            }, System.Windows.Threading.DispatcherPriority.Background);
+
+            //await operation.Task;
+        }
+
+        private static string CalculateData()
+        {
+            Thread.Sleep(2000);
+            return "Результат вычислений " + DateTime.Now.ToString();
         }
     }
 }
