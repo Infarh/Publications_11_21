422d2be34b534b22a23e077614f0a9f243396a1d
Infarh <shmachilin@yandex.ru>
2021-11-29 21:24:50 +0300

    Подключение БД к основному проекту

--- UI/Publications.MVC/Program.cs
+++ UI/Publications.MVC/Program.cs
@@ -1,6 +1,11 @@
+using Publications.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PublicationsDB>(opt => opt
+    .UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
