5b097b438d575475db1d8c55ff2b2183e26ed2d0
Infarh <shmachilin@yandex.ru>
2021-12-07 19:26:30 +0300

    Модель-представления главного окна

---
+++ UI/Publications.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Publications.WPF.ViewModels.Base;
+
+namespace Publications.WPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Главное окно программы";
+
+        public string Title { get => _Title; set => Set(ref _Title, value); }
+    }
+}
