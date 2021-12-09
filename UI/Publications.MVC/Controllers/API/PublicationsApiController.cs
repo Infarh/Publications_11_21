547facbe9cb3100a99ba3b0f2a52b08839da29e6
Infarh <shmachilin@yandex.ru>
2021-12-09 21:40:42 +0300

    Api-контроллер для доступа к репозиторию

---
+++ UI/Publications.MVC/Controllers/API/PublicationsApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Publications.Domain.Entities;
+using Publications.Interfaces.Repositories;
+
+namespace Publications.MVC.Controllers.API
+{
+    [ApiController, Route("api/publications")]
+    public class PublicationsApiController : ControllerBase
+    {
+        private readonly IRepository<Publication> _Repository;
+
+        public PublicationsApiController(IRepository<Publication> Repository) { _Repository = Repository; }
+
+        [HttpGet]
+        public async Task<IEnumerable<Publication>> GetAllAsync()
+        {
+            return await _Repository.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Publication?> GetAsync(int id)
+        {
+            return await _Repository.GetAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<int> AddAsync(Publication item)
+        {
+            return await _Repository.AddAsync(item);
+        }
+
+        [HttpPut]
+        public async Task<bool> UpdateAsync(Publication item)
+        {
+            return await _Repository.UpdateAsync(item);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _Repository.DeleteAsync(new() { Id = id });
+        }
+    }
+}
