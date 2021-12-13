f682f21051a88f6b537c2fe85b55e9497067f9fc
Infarh <shmachilin@yandex.ru>
2021-12-13 20:50:56 +0300

    Добавление хоста приложения в консоль

--- Tests/Publications.ConsoleTests/Program.cs
+++ Tests/Publications.ConsoleTests/Program.cs
@@ -1,11 +1,62 @@
 ﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Publications.DAL.Context;
+using Publications.Interfaces;
+using Publications.Services;
 
 namespace Publications.ConsoleTests;
 
 class Program
 {
-    public static void Main(string[] args)
+    private static IHost? __Host;
+
+    public static IHost Hosting => __Host ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+    public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+       .ConfigureHostConfiguration(opt => opt.AddJsonFile("otherconfigs.json", true, false))
+       .ConfigureAppConfiguration(opt => opt.AddJsonFile("otherconfigs.json", false, true))
+       .ConfigureAppConfiguration(opt => opt.AddXmlFile("appsettings.xml", true, true))
+       .ConfigureAppConfiguration(opt => opt.AddIniFile("appsettings.ini", true, true))
+       .ConfigureServices(ConfigureServices);
+
+    private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
     {
-        
+        services.AddDbContext<PublicationsDB>(opt => opt.UseSqlServer(host.Configuration.GetConnectionString("default")));
+
+        services.AddScoped<IPublicationManager, DatabasePublicationManager>();
+    }
+
+    public static async Task Main(string[] args)
+    {
+        using var host = Hosting;
+
+        await host.StartAsync();
+
+        // получаем главный сервис приложения и запускаем работу в нём (асинхронно).
+
+        await host.StopAsync();
+
+        //var service_collection = new ServiceCollection();
+        //service_collection.AddSingleton("Hello world!");
+        //service_collection.AddScoped<>()
+
+        //var services = service_collection.BuildServiceProvider();
+
+
+        //var ui = (IUserDialog)services.GetService(typeof(IUserDialog));
+        //var ui = services.GetService<IUserDalog>()
+        //var ui = services.GetRequiredService<IUserDialog>();
+        //var sp = services.GetRequiredService<IServiceProvider>();
+
+        //using (var scope = services.CreateScope())
+        //{
+        //    var scope_services = scope.ServiceProvider;
+        //}
+
+
     }
 }
