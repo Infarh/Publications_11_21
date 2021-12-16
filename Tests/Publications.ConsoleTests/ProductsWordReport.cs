23b490c6a3410ae57add0b344d8930026ec91af6
Infarh <shmachilin@yandex.ru>
2021-12-16 19:38:40 +0300

    Подготовка к формированию отчёта

---
+++ Tests/Publications.ConsoleTests/ProductsWordReport.cs
@@ -0,0 +1,34 @@
+﻿namespace Publications.ConsoleTests
+{
+    public class ProductsWordReport
+    {
+        private const string __CatalogNameField = "CatalogName";
+        private const string __ProductsCountField = "ProductsCount";
+        private const string __CreationDateField = "CreationDate";
+
+        private FileInfo _TemplateFile;
+
+        public string CatalogName { get; set; } = null!;
+
+        public int ProductsCount { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public ProductsWordReport(FileInfo TemplateFile) => _TemplateFile = TemplateFile;
+
+        public FileInfo Create(string ReportFile)
+        {
+            var report_file = new FileInfo(ReportFile);
+            if(report_file.Exists) 
+                report_file.Delete();
+
+            _TemplateFile.CopyTo(report_file.FullName);
+
+
+
+
+            report_file.Refresh();
+            return report_file;
+        }
+    }
+}
