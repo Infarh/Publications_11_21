6128cb518a8f6c6b418638b5e788620ff3455567
Infarh <shmachilin@yandex.ru>
2021-12-23 20:52:13 +0300

    Логика редактирования модели в контроллере авторов

--- UI/Publications.MVC/Controllers/AuthorsController.cs
+++ UI/Publications.MVC/Controllers/AuthorsController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
-
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Publications.Domain.Entities;
 using Publications.Interfaces.Repositories;
+using Publications.MVC.Infrastructure.Mapping;
 using Publications.MVC.ViewModels;
 
 namespace Publications.MVC.Controllers;
@@ -10,11 +11,13 @@ namespace Publications.MVC.Controllers;
 public class AuthorsController : Controller
 {
     private readonly IRepository<Person> _Persons;
+    private readonly IMapper _Mapper;
     private readonly ILogger<AuthorsController> _Logger;
 
-    public AuthorsController(IRepository<Person> Persons, ILogger<AuthorsController> Logger)
+    public AuthorsController(IRepository<Person> Persons, IMapper Mapper, ILogger<AuthorsController> Logger)
     {
         _Persons = Persons;
+        _Mapper = Mapper;
         _Logger = Logger;
     }
 
@@ -28,14 +31,18 @@ public class AuthorsController : Controller
         try
         {
             var authors = await _Persons.GetAllAsync();
-            var authors_view_models = authors.Select(
-                a => new AuthorViewModel
-                {
-                    Id = a.Id,
-                    LastName = a.LastName,
-                    Name = a.Name,
-                    Patronymic = a.Patronymic,
-                });
+            //var authors_view_models = authors.Select(
+            //    a => new AuthorViewModel
+            //    {
+            //        Id = a.Id,
+            //        LastName = a.LastName,
+            //        Name = a.Name,
+            //        Patronymic = a.Patronymic,
+            //    });
+
+            var authors_view_models = authors.ToView();
+
+            //var authors_view_models = _Mapper.Map<IEnumerable<AuthorViewModel>>(authors);
 
             return View(authors_view_models);
         }
@@ -45,4 +52,40 @@ public class AuthorsController : Controller
             throw;
         }
     }
+
+    public async Task<IActionResult> Edit(int Id)
+    {
+        try
+        {
+            var person = await _Persons.GetAsync(Id);
+            if (person is null)
+                return NotFound();
+
+            //return View(person.ToView());
+            return View(_Mapper.Map<AuthorViewModel>(person));
+        }
+        catch (Exception e)
+        {
+            LogError(e);
+            throw;
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(AuthorViewModel Model)
+    {
+        try
+        {
+            var person = Model.FromView()!;
+
+            await _Persons.UpdateAsync(person);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+        catch (Exception e)
+        {
+            LogError(e);
+            throw;
+        }
 }
