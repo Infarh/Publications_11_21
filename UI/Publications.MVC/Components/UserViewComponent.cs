6b5367a1e308bab656fbec91a7a9149afe50762c
Infarh <shmachilin@yandex.ru>
2021-12-27 19:21:26 +0300

    Визуальные компоненты

---
+++ UI/Publications.MVC/Components/UserViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Publications.MVC.Components
+{
+    //[ViewComponent]
+    public class UserViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => User.Identity!.IsAuthenticated
+            ? View("Registred")
+            : View();
+
+        //public async Task<IViewComponentResult> InvokeAsync() => View();
+    }
+}
